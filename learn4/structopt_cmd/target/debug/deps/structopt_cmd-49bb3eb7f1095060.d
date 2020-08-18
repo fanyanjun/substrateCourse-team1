d0699fea5bbe1dcedaaf36aeb1d0dfa63797dd66
yunplanet <jone988@protonmail.com>
2020-08-18 23:32:43 +0800

    add learn5

---
+++ learn4/structopt_cmd/target/debug/deps/structopt_cmd-49bb3eb7f1095060.d
@@ -0,0 +1,5 @@
+/Users/fanyanjun/rust/github.com/fanyanjun/substrateCourse-team1/learn4/structopt_cmd/target/debug/deps/structopt_cmd-49bb3eb7f1095060: src/main.rs
+
+/Users/fanyanjun/rust/github.com/fanyanjun/substrateCourse-team1/learn4/structopt_cmd/target/debug/deps/structopt_cmd-49bb3eb7f1095060.d: src/main.rs
+
+src/main.rs:
